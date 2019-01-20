40970a10e72c5e16d24ad28bd01f6f3cc40e8df7
Shane D Wilson <me@shanedwilson.com>
2019-01-20 11:59:59 -0600

    attempts logging in console and weather set in state

--- src/components/pages/Attempts/Attempts.js
+++ src/components/pages/Attempts/Attempts.js
@@ -1,8 +1,36 @@
 import React from 'react';
+import attemptsRequests from '../../../helpers/data/attemptsRequests'
+import weatherRequests from '../../../helpers/data/weatherRequests'
+import authRequests from '../../../helpers/data/authRequests';
 
 import './Attempts.scss';
 
 class Attempts extends React.Component {
+  state = {
+    attempts: [],
+    weather: [],
+  }
+
+  getAttempts = () => {
+    const uid = authRequests.getCurrentUid();
+    attemptsRequests.getAllAttempts(uid)
+    .then((attempts) => {
+      this.setState({ attempts });
+    })
+  };
+
+  getWeather = (position) => {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    weatherRequests.getCurrentWeather(lat, lon)
+      .then((weather) => {
+        this.setState({ weather })
+      })
+      .catch((err) => {
+        console.error('error with weather GET', err);
+      });  
+  } 
+
   addView = (e) => {
     const attemptId = e.target.id;
     this.props.history.push(`/attempts/${attemptId}/add`);
@@ -13,7 +41,15 @@ class Attempts extends React.Component {
     this.props.history.push(`/attempts/${attemptId}/edit`);
   } 
 
+  componentDidMount() {
+    this.getAttempts();
+    navigator.geolocation.getCurrentPosition(this.getWeather)    
+  }  
+
   render() {
+
+    console.log(this.state.attempts);
+
     return (
       <div className="Attempts mx-auto">
         <h1>ATTEMPTS!!!</h1>
