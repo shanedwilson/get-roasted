0bd273c8e8fc5bc6981a2050216d4a397564395c
Shane D Wilson <me@shanedwilson.com>
2019-01-20 11:42:32 -0600

    roasts logging in console

--- src/components/pages/Roasts/Roasts.js
+++ src/components/pages/Roasts/Roasts.js
@@ -1,14 +1,34 @@
 import React from 'react';
+import roastRequests from '../../../helpers/data/roastRequests'
+import authRequests from '../../../helpers/data/authRequests';
 
 import './Roasts.scss';
 
 class Roasts extends React.Component {
+  state = {
+    beans: [],
+  }
+
+  getRoasts = () => {
+    const uid = authRequests.getCurrentUid();
+    roastRequests.getAllRoasts(uid)
+    .then((roasts) => {
+      this.setState({ roasts });
+    })
+  };
+
   changeView = (e) => {
     const roastId = e.target.id;
     this.props.history.push(`/attempts/${roastId}`);
   }
 
+  componentDidMount() {
+    this.getRoasts();
+  }   
+
   render() {
+    console.log(this.state.roasts);
+
     return (
       <div className="Roasts mx-auto">
         <h1>ROASTS!!!</h1>
