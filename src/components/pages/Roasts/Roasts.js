9d81f9dac9bd189ae0ecd1275ac1b15c7ab7e992
Shane D Wilson <me@shanedwilson.com>
2019-01-19 20:26:18 -0600

    wip

---
+++ src/components/pages/Roasts/Roasts.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Roasts.scss';
+
+class Roasts extends React.Component {
+  render() {
+    return (
+      <div className="Roasts mx-auto">
+        <h1>ROASTS!!!</h1>
+      </div>
+    )
+  }
+}
+
+export default Roasts;
