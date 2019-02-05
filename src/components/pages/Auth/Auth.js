bcc5251e949314c94a29048d4d109409cd78fbdd
Shane D Wilson <me@shanedwilson.com>
2019-02-04 18:44:28 -0600

    wip

--- src/components/pages/Auth/Auth.js
+++ src/components/pages/Auth/Auth.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import $ from 'jquery';
 import authRequests from '../../../helpers/data/authRequests';
 import './Auth.scss';
 
 import googleButton from './images/googlebutton.png';
 import mainLogo from './images/get-roasted-logo1.png';
+
 
 class Auth extends React.Component {
   authenticateUser = (e) => {
@@ -15,12 +17,32 @@ class Auth extends React.Component {
       .catch(error => console.error('there was a problem with auth', error));
   }
 
+  setBackground = () => {
+    const bgArray = ['bean', 'roast', 'inventories', 'attempt'];
+    let counter = 0;
+    // if (counter === bgArray.length) { counter = 0; }
+
+    const nextBackground = () => {
+      $('body').addClass(bgArray[counter]);
+      setTimeout(nextBackground, 1000);
+      counter += 1;
+      if (counter === bgArray.length) { counter = 0; }
+      console.log(counter);
+    };
+    setTimeout(nextBackground, 1000);
+    $('body').addClass(bgArray[0]);
+  };
+
+  componentDidMount() {
+    this.setBackground();
+  }
+
   render() {
     return (
       <div className="Auth mx-auto">
         <div className="text-center logo-container mt-5">
           <div className="roasted-logo">
-            <img src={mainLogo} alt="main logo" />        
+            <img src={mainLogo} alt="main logo" />
           </div>
           <div className="title">
             <h1 className="title1">Get</h1>
@@ -28,7 +50,7 @@ class Auth extends React.Component {
           </div>
         </div>
         <div className="btn-container">
-          <button className="btn mt-5 mb-5" onClick={this.authenticateUser}>
+          <button className="btn mt-5 mb-5 mx-auto" onClick={this.authenticateUser}>
             <img src={googleButton} alt="google login button" />
           </button>
         </div>
